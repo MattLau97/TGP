fa253e04003bf3305f28d3d02050e6229a46e917
Matthew <matt.lau978@hotmail.com>
2018-10-25 12:22:56 +0100

    thursday 25th October_creation/editing

---
+++ Assets/Scripts/Emerge.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Emerge : MonoBehaviour {
+
+	public float speed;
+
+
+
+	// Use this for initialization
+	void Start () {
+		transform.position = new Vector3 (0, 10, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
